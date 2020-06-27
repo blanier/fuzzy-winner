e7e86544f3c9fcb1d3862c6844fb2c626f4e014f
Brian Lanier <lanier.brian@gmail.com>
2020-06-26 19:20:29 -0700

    add a "Clear Console" button

--- init.lua
+++ init.lua
@@ -96,6 +96,21 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "1", function()
 
 end)
 
+-- custom Console toolbar (adds Clear button)
+local toolbar = require("hs.webview.toolbar")
+local console = require("hs.console")
+local image = require("hs.image")
+console.defaultToolbar = toolbar.new("CustomToolbar", {
+    { id="prefs", label="Preferences", image=image.imageFromName("NSPreferencesGeneral"), tooltip="Open Preferences", fn=function() hs.openPreferences() end },
+    { id = "NSToolbarSpaceItem" },
+    { id="reload", label="Reload config", image=image.imageFromName("NSSynchronize"), tooltip="Reload configuration", fn=function() hs.reload() end },
+    { id="openCfg", label="Open config", image=image.imageFromName("NSActionTemplate"), tooltip="Edit configuration", fn=function() openConfig() end },
+    { id="clearLog", label="Clear Console", image = hs.image.imageFromName("NSTrashEmpty"), tooltip="Clear Console", fn=function() console.clearConsole() end },
+    { id = "NSToolbarFlexibleSpaceItem" },
+    { id="help", label="Help", image=image.imageFromName("NSInfo"), tooltip="Open API docs browser", fn=function() hs.doc.hsdocs.help() end }
+  }):canCustomize(true):autosaves(true)
+console.toolbar(console.defaultToolbar)
+
 -- Handle reloading the config w/o user intervention
 function reloadConfig(files)
     doReload = false
